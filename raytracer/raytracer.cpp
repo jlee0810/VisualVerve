d7ad3a5399359c295b610f659f755bf65310382c
Joonwon (Eric) Lee <joonwontech@gmail.com>
2023-07-25 23:11:33 +0900

    made multiple spheres (considered depth)

--- raytracer/raytracer.cpp
+++ raytracer/raytracer.cpp
@@ -10,11 +10,12 @@ struct Sphere
     // Define the center and radius of the sphere
     Vec3f center;
     float radius;
+    Vec3f color;
 
-    Sphere(const Vec3f &c, const float &r) : center(c), radius(r) {}
+    Sphere(const Vec3f &c, const float &r, const Vec3f &color) : center(c), radius(r), color(color) {}
 
     // intersecting algorithm: http://www.lighthouse3d.com/tutorials/maths/ray-sphere-intersection/
-    bool ray_intersect(const Vec3f &p, const Vec3f &dir, float &t0) const
+    bool ray_intersect(const Vec3f &p, const Vec3f &dir, float &closestDist) const
     {
         Vec3f vpc = center - p;
         float projection = vpc * dir;
@@ -24,32 +25,58 @@ struct Sphere
         if (disToRay > radius * radius)
             return false;
         float t0todisToRay = sqrtf((radius * radius) - disToRay);
-        t0 = projection - t0todisToRay;
+        float t0 = projection - t0todisToRay;
         float t1 = projection + t0todisToRay;
         if (t0 < 0)
             t0 = t1;
         if (t0 < 0)
             return false;
+
+        if (t0 < closestDist)
+        {
+            closestDist = t0;
+            return true;
+        }
         return true;
     }
 };
 
-Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const Sphere &sphere)
+Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &scene)
 {
     float sphere_dist = std::numeric_limits<float>::max();
-    if (!sphere.ray_intersect(orig, dir, sphere_dist))
+    Vec3f fillColor{};
+    bool filled = false;
+
+    for (const Sphere &s : scene)
     {
-        return Vec3f(0.2, 0.7, 0.8); // background color
+        if (s.ray_intersect(orig, dir, sphere_dist))
+        {
+            fillColor = s.color;
+            filled = true;
+        }
     }
-    return Vec3f(0.4, 0.4, 0.3);
+
+    if (!filled)
+        return Vec3f(0.3, 0.3, 0.3);
+    else
+        return fillColor;
 }
 
-void render(const Sphere &sphere)
+void render()
 {
     const int width = 1024;
     const int height = 768;
     std::vector<Vec3f> framebuffer(width * height);
     const int fov = M_PI / 2.;
+
+    Vec3f babyBlue = {0.537, 0.812, 0.941};
+    Vec3f babyPink = {0.973, 0.537, 0.537};
+
+    std::vector<Sphere> scene;
+    scene.push_back(Sphere(Vec3f(-3, 0, -16), 2, babyBlue));
+    scene.push_back(Sphere(Vec3f(-1.0, -1.5, -12), 2, babyPink));
+    scene.push_back(Sphere(Vec3f(1.5, -0.5, -18), 3, babyBlue));
+    scene.push_back(Sphere(Vec3f(7, 5, -18), 4, babyPink));
 
     for (size_t j = 0; j < height; j++)
     {
@@ -58,7 +85,7 @@ void render(const Sphere &sphere)
             float x = (2 * (i + 0.5) / (float)width - 1) * tan(fov / 2.) * width / (float)height;
             float y = -(2 * (j + 0.5) / (float)height - 1) * tan(fov / 2.);
             Vec3f dir = Vec3f(x, y, -1).normalize();
-            framebuffer[i + j * width] = cast_ray(Vec3f(0, 0, 0), dir, sphere);
+            framebuffer[i + j * width] = cast_ray(Vec3f(0, 0, 0), dir, scene); // 카메라는 0,0,0에 위치
         }
     }
 
@@ -78,8 +105,7 @@ void render(const Sphere &sphere)
 
 int main()
 {
-    Sphere sphere(Vec3f(-3, 0, -16), 2);
-    render(sphere);
+    render();
 
     return 0;
 }
