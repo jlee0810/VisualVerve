eca507d0d325d3b3a0c712c71799d1adf9a2cbea
Joonwon (Eric) Lee <joonwontech@gmail.com>
2023-08-03 22:09:21 +0900

    Ray Marching Art Work
    Initial commit with diffuse lighting

---
+++ raymarching/build/CMakeFiles/tinykaboom.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tinykaboom.dir/raymarching.cpp.o"
+  "CMakeFiles/tinykaboom.dir/raymarching.cpp.o.d"
+  "tinykaboom"
+  "tinykaboom.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tinykaboom.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
