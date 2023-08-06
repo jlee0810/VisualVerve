d837d05f379a55b948fb781a70645bbf6669b585
Joonwon (Eric) Lee <joonwontech@gmail.com>
2023-08-06 22:31:33 +0900

    Added Perlin Noise and Fire color

--- raymarching/raymarching.cpp
+++ raymarching/raymarching.cpp
@@ -7,15 +7,84 @@
 #include <vector>
 #include "geometry.h"
 
-const float sphere_radius = 1.5;
+float sphere_radius = 1;
+const float noise_amplitude = 1.0;
+
+template <typename T>
+inline T lerp(const T &v0, const T &v1, float t) // Linear Interpolation
+{
+    // lerp(a, b, x) = a + (b -a ) * x;
+    return v0 + (v1 - v0) * std::max(0.f, std::min(1.f, t));
+}
+
+float hash(const float n)
+{
+    float x = sin(n) * 43758.5453f;
+    return x - floor(x);
+}
+
+float noise(const Vec3f &x)
+{
+    Vec3f p(floor(x.x), floor(x.y), floor(x.z));
+    Vec3f f(x.x - p.x, x.y - p.y, x.z - p.z);
+    f = f * (f * (Vec3f(3.f, 3.f, 3.f) - f * 2.f));
+    float n = p * Vec3f(1.f, 57.f, 113.f);
+    return lerp(lerp(
+                    lerp(hash(n + 0.f), hash(n + 1.f), f.x),
+                    lerp(hash(n + 57.f), hash(n + 58.f), f.x), f.y),
+                lerp(
+                    lerp(hash(n + 113.f), hash(n + 114.f), f.x),
+                    lerp(hash(n + 170.f), hash(n + 171.f), f.x), f.y),
+                f.z);
+}
+
+Vec3f rotate(const Vec3f &v)
+{
+    return Vec3f(Vec3f(0.00, 0.80, 0.60) * v, Vec3f(-0.80, 0.36, -0.48) * v, Vec3f(-0.60, -0.48, 0.64) * v);
+}
+
+float fractal_brownian_motion(const Vec3f &x)
+{
+    Vec3f p = rotate(x);
+    float f = 0;
+    f += 0.5000 * noise(p);
+    p = p * 2.32;
+    f += 0.2500 * noise(p);
+    p = p * 3.03;
+    f += 0.1250 * noise(p);
+    p = p * 2.61;
+    f += 0.0625 * noise(p);
+    return f / 0.9375;
+}
+
+Vec3f fire_color(const float d)
+{
+    const Vec3f yellow(1.7, 1.3, 1.0); // note that the color is "hot", i.e. has components >1
+    const Vec3f orange(1.0, 0.6, 0.0);
+    const Vec3f red(1.0, 0.0, 0.0);
+    const Vec3f darkgray(0.2, 0.2, 0.2);
+    const Vec3f gray(0.4, 0.4, 0.4);
+
+    float x = std::max(0.f, std::min(1.f, d));
+    if (x < .25f)
+        return lerp(gray, darkgray, x * 4.f);
+    else if (x < .5f)
+        return lerp(darkgray, red, x * 4.f - 1.f);
+    else if (x < .75f)
+        return lerp(red, orange, x * 4.f - 2.f);
+    return lerp(orange, yellow, x * 4.f - 3.f);
+}
 
 float signed_distance(const Vec3f &p)
 {
-    return p.norm() - sphere_radius;
+    float displacement = -fractal_brownian_motion(p * 3.4) * noise_amplitude;
+    return p.norm() - (sphere_radius + displacement);
 }
 
 bool sphere_trace(const Vec3f &orig, const Vec3f &dir, Vec3f &pos)
 {
+    if (orig * orig - pow(orig * dir, 2) > pow(sphere_radius, 2))
+        return false;
     pos = orig;
     for (size_t i = 0; i < 128; i++)
     {
@@ -40,45 +109,57 @@ Vec3f distance_field_normal(const Vec3f &pos)
 
 int main()
 {
-    const int width = 720;
-    const int height = 720;
+    const int width = 640;
+    const int height = 480;
     const float fov = M_PI / 3.;
-    std::vector<Vec3f> framebuffer(width * height);
+    const int frames_per_second = 24;
+    const int total_seconds = 10;
+    const int total_frames = frames_per_second * total_seconds;
+    const float start_radius = 1;
+    const float end_radius = 2.5;
+
+    for (int frame = 0; frame < total_frames; frame++)
+    {
+        float t = (float)frame / (total_frames - 1);
+        sphere_radius = lerp(start_radius, end_radius, t); // Linear interpolation
+
+        std::vector<Vec3f> framebuffer(width * height);
 
 #pragma omp parallel for
-    for (size_t j = 0; j < height; j++)
-    { // actual rendering loop
-        for (size_t i = 0; i < width; i++)
+        for (size_t j = 0; j < height; j++)
         {
-            float dir_x = (i + 0.5) - width / 2.;
-            float dir_y = -(j + 0.5) + height / 2.; // this flips the image at the same time
-            float dir_z = -height / (2. * tan(fov / 2.));
-            Vec3f hit;
-            if (sphere_trace(Vec3f(0, 0, 3), Vec3f(dir_x, dir_y, dir_z).normalize(), hit))
-            { // the camera is placed to (0,0,3) and it looks along the -z axis
-                Vec3f light_dir = (Vec3f(10, 10, 10) - hit).normalize();
-                //add diffuse lighting
-                float light_intensity = std::max(0.4f, light_dir * distance_field_normal(hit));
-                framebuffer[i + j * width] = Vec3f(light_intensity, light_intensity, light_intensity);
-            }
-            else
+            for (size_t i = 0; i < width; i++)
             {
-                framebuffer[i + j * width] = Vec3f(0.2, 0.7, 0.8); // background color
+                float dir_x = (i + 0.5) - width / 2.;
+                float dir_y = -(j + 0.5) + height / 2.;
+                float dir_z = -height / (2. * tan(fov / 2.));
+                Vec3f hit;
+                if (sphere_trace(Vec3f(0, 0, 3), Vec3f(dir_x, dir_y, dir_z).normalize(), hit))
+                {
+                    float noise_level = (sphere_radius - hit.norm()) / noise_amplitude;
+                    Vec3f light_dir = (Vec3f(10, 10, 10) - hit).normalize();
+                    float light_intensity = std::max(0.4f, light_dir * distance_field_normal(hit));
+                    framebuffer[i + j * width] = fire_color((-.2 + noise_level) * 2) * light_intensity;
+                }
+                else
+                {
+                    framebuffer[i + j * width] = Vec3f(0.2, 0.7, 0.8); // background color
+                }
             }
         }
-    }
 
-    std::ofstream ofs("./out.ppm", std::ios::binary); // save the framebuffer to file
-    ofs << "P6\n"
-        << width << " " << height << "\n255\n";
-    for (size_t i = 0; i < height * width; ++i)
-    {
-        for (size_t j = 0; j < 3; j++)
+        std::ofstream ofs("./out_" + std::to_string(frame) + ".ppm", std::ios::binary);
+        ofs << "P6\n"
+            << width << " " << height << "\n255\n";
+        for (size_t i = 0; i < height * width; ++i)
         {
-            ofs << (char)(std::max(0, std::min(255, static_cast<int>(255 * framebuffer[i][j]))));
+            for (size_t j = 0; j < 3; j++)
+            {
+                ofs << (char)(std::max(0, std::min(255, static_cast<int>(255 * framebuffer[i][j]))));
+            }
         }
+        ofs.close();
     }
-    ofs.close();
 
     return 0;
 }
