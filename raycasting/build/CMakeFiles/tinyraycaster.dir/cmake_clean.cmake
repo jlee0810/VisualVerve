2022ade928a737e12ab772fad6d16409fa7ba98f
Joonwon (Eric) Lee <joonwontech@gmail.com>
2023-08-08 21:52:42 +0900

    Raycast first person perspective of maze

---
+++ raycasting/build/CMakeFiles/tinyraycaster.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tinyraycaster.dir/raycaster.cpp.o"
+  "CMakeFiles/tinyraycaster.dir/raycaster.cpp.o.d"
+  "tinyraycaster"
+  "tinyraycaster.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tinyraycaster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ raycasting/build/CMakeFiles/tinyraycaster.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for tinyraycaster.
+# This may be replaced when dependencies are built.
