70f536b08f7ec1fcc9470ff1671a233b7e8888b6
Joonwon (Eric) Lee <joonwontech@gmail.com>
2023-08-11 22:07:19 +0900

    added temporary sprites

--- raycasting/raycaster.cpp
+++ raycasting/raycaster.cpp
@@ -12,23 +12,60 @@
 #include "player.h"
 #include "framebuffer.h"
 #include "textures.h"
+#include "sprite.h"
 
-int wall_x_texcoord(const float x, const float y, Texture &tex_walls)
+int wall_x_texcoord(const float hitx, const float hity, Texture &tex_walls)
 {
-    float hitx = x - floor(x + .5); // hitx and hity contain (signed) fractional parts of x and y,
-    float hity = y - floor(y + .5); // they vary between -0.5 and +0.5, and one of them is supposed to be very close to 0
-    int tex = hitx * tex_walls.size;
-    if (std::abs(hity) > std::abs(hitx)) // we need to determine whether we hit a "vertical" or a "horizontal" wall (w.r.t the map)
-        tex = hity * tex_walls.size;
+    float x = hitx - floor(hitx + .5);
+    float y = hity - floor(hity + .5);
+
+    int tex = x * tex_walls.size;
+    if (std::abs(y) > std::abs(x)) // we need to determine whether we hit a "vertical" or a "horizontal" wall (w.r.t the map)
+        tex = y * tex_walls.size;
     if (tex < 0) // do not forget x_texcoord can be negative, fix that
         tex += tex_walls.size;
     assert(tex >= 0 && tex < (int)tex_walls.size);
     return tex;
 }
 
-void render(FrameBuffer &fb, Map &map, Player &player, Texture &tex_walls)
+void map_sprite(Sprite &sprite, FrameBuffer &fb, Map &map)
 {
-    fb.clear(pack_color(255, 255, 255)); // clear the screen
+    const size_t map_cell_w = fb.w / (map.w * 2); // map cell width
+    const size_t map_cell_h = fb.h / map.h;       // map cell height
+    fb.draw_rectangle(sprite.x * map_cell_w - 3, sprite.y * map_cell_h - 3, 6, 6, pack_color(255, 0, 0));
+}
+
+void draw_sprite(Sprite &sprite, FrameBuffer &fb, Player &player, Texture &tex_sprites)
+{
+    float sprite_dir = atan2(sprite.y - player.y, sprite.x - player.x); // angle between sprite and player
+
+    // keep the angle between -pi and pi by removing uncessary periods
+    while (sprite_dir - player.angle > M_PI)
+        sprite_dir -= 2 * M_PI;
+    while (sprite_dir - player.angle < -M_PI)
+        sprite_dir += 2 * M_PI;
+
+    float sprite_dist = std::sqrt(pow(player.x - sprite.x, 2) + pow(player.y - sprite.y, 2));                            // distance from the player to the sprite
+    size_t sprite_screen_size = std::min(1000, static_cast<int>(fb.h / sprite_dist));                                    // screen sprite size
+    int h_offset = (sprite_dir - player.angle) / player.player_fov * (fb.w / 2) + (fb.w / 2) / 2 - tex_sprites.size / 2; // do not forget the 3D view takes only a half of the framebuffer
+    int v_offset = fb.h / 2 - sprite_screen_size / 2;
+
+    for (size_t i = 0; i < sprite_screen_size; i++)
+    {
+        if (h_offset + i < 0 || h_offset + i >= fb.w / 2)
+            continue;
+        for (size_t j = 0; j < sprite_screen_size; j++)
+        {
+            if (v_offset + j < 0 || v_offset + j >= fb.h)
+                continue;
+            fb.set_pixel(fb.w / 2 + h_offset + i, v_offset + j, pack_color(0, 0, 0));
+        }
+    }
+}
+
+void render(FrameBuffer &fb, Map &map, Player &player, std::vector<Sprite> &sprites, Texture &tex_walls, Texture &tex_monst)
+{
+    fb.clear(pack_color(255, 255, 255)); // clear the screen with white
 
     const size_t rect_w = fb.w / (map.w * 2); // size of one map cell on the screen
     const size_t rect_h = fb.h / map.h;
@@ -60,7 +97,10 @@ void render(FrameBuffer &fb, Map &map, Player &player, Texture &tex_walls)
 
             size_t texid = map.get(x, y); // our ray touches a wall, so draw the vertical column to create an illusion of 3D
             assert(texid < tex_walls.count);
-            size_t column_height = fb.h / (t * cos(angle - player.angle));
+
+            float dist = t * cos(angle - player.angle); // remove fish eye
+            size_t column_height = fb.h / dist;
+
             int x_texcoord = wall_x_texcoord(x, y, tex_walls);
             std::vector<uint32_t> column = tex_walls.get_scaled_column(texid, x_texcoord, column_height);
             int pix_x = i + fb.w / 2; // we are drawing at the right half of the screen, thus +fb.w/2
@@ -75,6 +115,11 @@ void render(FrameBuffer &fb, Map &map, Player &player, Texture &tex_walls)
             break;
         } // ray marching loop
     }     // field of view ray sweeping
+    for (size_t i = 0; i < sprites.size(); i++)
+    {
+        map_sprite(sprites[i], fb, map);
+        draw_sprite(sprites[i], fb, player, tex_monst);
+    }
 }
 
 int main()
@@ -82,22 +127,26 @@ int main()
     FrameBuffer fb{1024, 512, std::vector<uint32_t>(1024 * 512, pack_color(255, 255, 255))};
     Player player{3.456, 2.345, 1.523, M_PI / 3.};
     Map map;
+
     Texture tex_walls("../walltext.png");
-    if (!tex_walls.count)
+    Texture tex_monst("../monsters.png");
+
+    if (!tex_walls.count || !tex_monst.count)
     {
-        std::cerr << "Failed to load wall textures" << std::endl;
+        if (!tex_walls.count)
+            std::cerr << "Failed to load wall textures" << std::endl;
+        if (!tex_monst.count)
+            std::cerr << "Failed to load monster textures" << std::endl;
         return -1;
     }
 
-    for (size_t frame = 0; frame < 360; frame++)
-    {
-        std::stringstream ss;
-        ss << std::setfill('0') << std::setw(5) << frame << ".ppm";
-        player.angle += 2 * M_PI / 360;
+    std::vector<Sprite> sprites;
+    sprites.push_back({1.834, 8.765, 0}); // add some random sprites to the map
+    sprites.push_back({2.764, 7.345, 1}); // they have random positions and directions
+    sprites.push_back({2.000, 2.000, 1}); // they have random positions and directions
 
-        render(fb, map, player, tex_walls);
-        drop_ppm_image(ss.str(), fb.img, fb.w, fb.h);
-    }
+    render(fb, map, player, sprites, tex_walls, tex_monst);
+    drop_ppm_image("./out.ppm", fb.img, fb.w, fb.h);
 
     return 0;
 }
